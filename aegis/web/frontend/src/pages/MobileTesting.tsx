98335db0dd09e2a4e91972fb6cb8e95fab5e4072
Saiteja <saitejareddy773@gmail.com>
2026-07-08 20:32:24 +0530

    fix: mobile testing upload form cut off, change overflow-hidden to overflow-y-auto

--- aegis/web/frontend/src/pages/MobileTesting.tsx
+++ aegis/web/frontend/src/pages/MobileTesting.tsx
@@ -251,7 +251,7 @@ export default function MobileTesting() {
       </div>
 
       {/* Main Content */}
-      <div className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex-1 flex flex-col overflow-y-auto">
         {view === 'upload' ? (
           <UploadView
             onUpload={async (app) => {
